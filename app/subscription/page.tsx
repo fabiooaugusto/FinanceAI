b05de96e5e01793be3dbaee095f174131aff7fcf
fabiooaugusto <fabio-augusto04@outlook.com>
2024-11-07 00:55:47 -0300

    feat: add login page

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,5 @@
-const Subscription = () => {
-    return ( <h1>Subscription Page</h1> );
-}
- 
-export default Subscription;
+const SubscriptionPage = () => {
+  return <h1>Subscription Page</h1>;
+};
+
+export default SubscriptionPage;
